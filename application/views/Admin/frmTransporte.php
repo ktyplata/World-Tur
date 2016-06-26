c3a560698d42569fe3821676d13d1f152f25dc9a
ktyplata <ktyplatacoffi13@gmail.com>
2016-06-25 21:27:32 -0500

    frmTransporte

--- application/views/Admin/frmTransporte.php
+++ application/views/Admin/frmTransporte.php
@@ -13,15 +13,15 @@
         <label for="NumLugares" >Número Lugares </label>
     <div class="form-group input-group">
          <span class="input-group-addon"><i class="fa fa-user"></i> </span>
-         <input class="form-control" type="text" id="NumLugares" name="NumLugares" placeholder="Número Lugares" required="required" value="<?php echo set_value('Nombre'); ?>">
+         <input class="form-control" type="number" id="NumLugares" name="NumLugares" placeholder="Número Lugares" required="required" value="<?php echo set_value('Nombre'); ?>">
         
     </div>
         
         
         <label  for="NomTransporte">Nombre Transporte</label>
     <div class="form-group input-group">
-         <span class="input-group-addon"><i class="fa fa-phone"></i> </span>  
-         <input class="form-control" type="tel" id="NomTransporte" name="NomTransporte" placeholder="Nombre Transporte" required="required">
+         <span class="input-group-addon"><i class="fa fa-bus"></i> </span>  
+         <input class="form-control" type="text" id="NomTransporte" name="NomTransporte" placeholder="Nombre Transporte" required="required">
     </div>
        
           
