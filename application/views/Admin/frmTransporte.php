a2800f8bdddf4d065b63214bf511822c12f36054
ktyplata <ktyplatacoffi13@gmail.com>
2016-06-25 21:14:35 -0500

    Vista frmTransporte

--- application/views/Admin/frmTransporte.php
+++ application/views/Admin/frmTransporte.php
@@ -1,8 +1,40 @@
-<?php
+<div class="col-sm-6 col-sm-offset-2" >
+    
+    
+    
+<h1 class="mbr-header__text">Registra un Transporte</h1>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        
+<?php echo form_open('Transporte/addTransporte'); ?>
+<form>
+    
+   
+ 
+        <label for="NumLugares" >Número Lugares </label>
+    <div class="form-group input-group">
+         <span class="input-group-addon"><i class="fa fa-user"></i> </span>
+         <input class="form-control" type="text" id="NumLugares" name="NumLugares" placeholder="Número Lugares" required="required" value="<?php echo set_value('Nombre'); ?>">
+        
+    </div>
+        
+        
+        <label  for="NomTransporte">Nombre Transporte</label>
+    <div class="form-group input-group">
+         <span class="input-group-addon"><i class="fa fa-phone"></i> </span>  
+         <input class="form-control" type="tel" id="NomTransporte" name="NomTransporte" placeholder="Nombre Transporte" required="required">
+    </div>
+       
+          
+    <div class="form-group">
+           <input type="submit"  name="submit"  value="Agregar" class="btn btn-success btn-lg"  required="required" >
+    </div>
+    
+</form>
+
+                     
+                          
+                       
+
+                   
+</div>
 
