f2e6c287b57a2aa90214843f674d95def6618c5d
ktyplata <ktyplatacoffi13@gmail.com>
2016-06-29 12:25:27 -0500

    Edición vista frmProcedencia

--- WT/application/views/Admin/frmProcedencia.php
+++ WT/application/views/Admin/frmProcedencia.php
@@ -5,23 +5,30 @@
 <h1 class="mbr-header__text">Registra un Transporte</h1>
 
         
-<?php echo form_open('Transporte/addTransporte'); ?>
+<?php echo form_open('Procedencia/addProcedencia'); ?>
 <form>
     
    
  
-        <label for="NumLugares" >Número Lugares </label>
+        <label for="LugarSalida" >LugarSalida </label>
     <div class="form-group input-group">
          <span class="input-group-addon"><i class="fa fa-user"></i> </span>
-         <input class="form-control" type="number" id="NumLugares" name="NumLugares" placeholder="Número Lugares" required="required" value="<?php echo set_value('Nombre'); ?>">
+         <input class="form-control" type="text" id="LugarSalida" name="LugarSalida" placeholder="Lugar Salida" required="required" value="<?php echo set_value('Nombre'); ?>">
         
     </div>
         
         
-        <label  for="NomTransporte">Nombre Transporte</label>
+        <label  for="HorarioSalida">HorarioSalida</label>
     <div class="form-group input-group">
          <span class="input-group-addon"><i class="fa fa-bus"></i> </span>  
-         <input class="form-control" type="text" id="NomTransporte" name="NomTransporte" placeholder="Nombre Transporte" required="required">
+         <input class="form-control" type="datetime" id="HorarioSalida" name="HorarioSalida" placeholder="Horario Salida" required="required">
+    </div>
+        
+        
+        <label  for="idProveedor">idProveedor</label>
+    <div class="form-group input-group">
+         <span class="input-group-addon"><i class="fa fa-user-secret"></i> </span>  
+         <input class="form-control" type="number" id="idProveedor" name="idProveedor" placeholder="idProveedor" required="required">
     </div>
        
           
