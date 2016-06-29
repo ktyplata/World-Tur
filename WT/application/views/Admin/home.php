c3ca244d14d06e1873c02af0b7d1d06e3bbec17e
ktyplata <ktyplatacoffi13@gmail.com>
2016-06-28 23:32:12 -0500

    j

--- WT/application/views/Admin/home.php
+++
@@ -1,8 +0,0 @@
-
-<h1 style="float: left; color: wheat; margin-left: 5%;"> Bienvenid@ <?php  echo $this->session->userdata('user'); 
-	?> al CPanel  </h1> 
-
-
-<a  href="<?php echo base_url();?>index.php/Usuarios/cerrarSesion"><button  class="btn btn-warning" style="color: white; margin-top: 10px; float: right; margin-right: 5%;"> Cerrar Sesion</button></a>
-
-<br>
--- WT/application/views/plantillaFrond.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$this->load->view('Plantilla_Frond-End/begin.php');
-$this->load->view('Plantilla_Frond-End/header');
-$this->load->view($content);
-$this->load->view('Plantilla_Frond-End/nav');
-$this->load->view('Plantilla_Frond-End/footer');
-$this->load->view('Plantilla_Frond-End/end');
