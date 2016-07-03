cbcaf48cceba2b3983d5d1a56e59884481a437af
tavoguerra1916 <tavoguerra2008@hotmail.com.es>
2016-07-03 00:00:21 -0500

    Mejora Libreria Calendar 2

---
+++ WT/application/controllers/Principal.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Principal extends CI_Controller{
+    
+     public function __construct() {
+        parent::__construct();
+        $this->load->library('calendar');
+    }
+      public function index()
+    {
+           $data ['calendario'] = $this->calendar->generate();
+        $data['content'] = 'Cliente/home';
+        $this->load->view('plantillaFrond', $data);
+        
+    }
+    
+    
+    public function conocenos()
+    {
+        $data['content'] = 'Cliente/conocenos';
+        $this->load->view('plantillaFrond', $data);
+    }
+    
+     public function contactanos()
+    {
+        $data['content'] = 'Cliente/contactanos';
+        $this->load->view('plantillaFrond', $data);
+    }
+    
+     public function faq()
+    {
+        $data['content'] = 'Cliente/faq';
+        $this->load->view('plantillaFrond', $data);
+    }
+    
+     public function galeria()
+    {
+        $data['content'] = 'Cliente/galeria';
+        $this->load->view('plantillaFrond', $data);
+    }
+    
+    
+}
